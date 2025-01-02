ae628df675511657a850c035e77a8f06e9ff058e
kentarokobayashi <happydesign.0922@gmail.com>
2025-01-02 13:35:44 +0900

    一旦コミット

--- src/jsx/lms-variable.js
+++ src/jsx/lms-variable.js
@@ -1,32 +1,37 @@
-// 本番テナントの変数定義
+// 検証テナントの変数定義
 const SubjectIds = {
-    SubjectMain: {
-        philosophy: { id: 9999, name: '哲学' },
-        science: { id: 9999, name: '科学' },
-        economy: { id: 9999, name: '経済' },
-        ThreeSubjectPack: { id: 9999, name: '3科目セット' },
-        TwoSubjectPack: { id: 9999, name: '2科目セット' },
-    },
-    SubjectChild: {
-        philosophy: {
-            L1: { id: 9999, name: '哲学 L1' },
-            L2: { id: 9999, name: '哲学 L2' },
-            L3: { id: 9999, name: '哲学 L3' },
-            L4: { id: 9999, name: '哲学 L4' }
-        },
-        science: {
-            L1: { id: 9999, name: '科学 L1' },
-            L2: { id: 9999, name: '科学 L2' },
-            L3: { id: 9999, name: '科学 L3' },
-            L4: { id: 9999, name: '科学 L4' }
-        },
-        economy: {
-            L1: { id: 9999, name: '経済 L1' },
-            L2: { id: 9999, name: '経済 L2' },
-            L3: { id: 9999, name: '経済 L3' },
-            L4: { id: 9999, name: '経済 L4' }
-        },
-    },
-    GlobalEnglish: { id: 9999, name: 'グローバル英語' },
-    Programming: { id: 9999, name: 'プログラミング' }
+  SubjectMain: {
+      philosophy: { id: 999, name: '哲学' },
+      science: { id: 999, name: '科学' },
+      economy: { id: 999, name: '経済' },
+      ThreeSubjectPack: { id: 999, name: '3科目セット' },
+      TwoSubjectPack: { id: 999, name: '2科目セット' },
+      GlobalEnglish: { id: 999, name: 'グローバル英語' },
+  },
+  SubjectChild: {
+      philosophy: {
+          ph_L1: { id: 999, name: '哲学 L1' },
+          ph_L2: { id: 999, name: '哲学 L2' },
+          ph_L3: { id: 999, name: '哲学 L3' },
+          ph_L4: { id: 999, name: '哲学 L4' }
+      },
+      science: {
+          sc_L1: { id: 999, name: '科学 L1' },
+          sc_L2: { id: 999, name: '科学 L2' },
+          sc_L3: { id: 999, name: '科学 L3' },
+          sc_L4: { id: 999, name: '科学 L4' }
+      },
+      economy: {
+          ec_L1: { id: 999, name: '経済 L1' },
+          ec_L2: { id: 999, name: '経済 L2' },
+          ec_L3: { id: 999, name: '経済 L3' },
+          ec_L4: { id: 999, name: '経済 L4' }
+      },
+      GlobalEnglish: {
+          en_L1: { id: 999, name: 'グローバル英語 L1' },
+          en_L2: { id: 999, name: 'グローバル英語 L2' },
+      },
+  },
+ 
+  Programming: { id: 999, name: 'プログラミング' }
 };
