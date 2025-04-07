45264cc9c4fc267e8edce7d59bb5436315912d3c
kentaro kobayashi <kentarokobayashi@MB249FE68F.local>
2025-04-07 11:47:11 +0900

    英語のみ15日まで延長

--- static/js/stg.js
+++ static/js/stg.js
@@ -1304,8 +1304,15 @@ if (
 // カード登録後にモーダルを表示する関数
 function showModalAfterCardRegistration() {
   var now = new Date(); // 現在の日付を取得
-  var campaignEnd = new Date(2025, 3, 5, 23, 59, 59); // キャンペーン終了日時（2025年4月5日23:59:59）
+  var campaignEnd = new Date(2025, 3, 15, 23, 59, 59); // キャンペーン終了日時（2025年4月5日23:59:59）
   var cookieValue = $.cookie("levelSettingModalShown"); // Cookieにモーダル表示の履歴があるか確認
+  var subjectCategory = currentViewCourseData.key;  // 現在選択されている科目カテゴリーを取得
+
+  // globalenglish 以外なら即座に通常モーダルだけ表示
+  if (subjectCategory !== "globalenglish") {
+    showLevelSettingModal();
+    return;
+  }
 
   // ".c-modal-level-setting"クラスの要素がクリックされた場合にレベル設定モーダルを表示
   $(document).on("click", ".c-modal-level-setting", function () {
