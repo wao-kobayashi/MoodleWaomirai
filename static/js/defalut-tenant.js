5baca4611ab2e5217b97ef39b21f7de5b17e45e5
kentarokobayashi <happydesign.0922@gmail.com>
2025-10-02 11:13:48 +0900

    順番を最新版に

--- static/js/defalut-tenant.js
+++ static/js/defalut-tenant.js
@@ -10,21 +10,21 @@ $(document).ready(function () {
   if (tenantId === 1) {
     const bodyId = $("body").attr("id");
 
-    //page-login-index：ログイン画面
     //page-site-index：サイトのホーム画面
+    //page-login-index：ログイン画面
     //page-login-signup：アカウント作成画面
     if (
-      bodyId === "page-login-index" ||
       bodyId === "page-site-index" ||
+      bodyId === "page-login-index" ||
       bodyId === "page-login-signup"
     ) {
       // bodyIdに応じてリンク先を設定
       let linkUrl = "https://lms.waomirai.com/?tenant=lmswaomirai"; // デフォルト
 
-      if (bodyId === "page-login-index") {
-        linkUrl = "https://lms.waomirai.com/login/index.php?tenant=lmswaomirai";
-      } else if (bodyId === "page-site-index") {
+      if (bodyId === "page-site-index") {
         linkUrl = "https://lms.waomirai.com?tenant=lmswaomirai";
+      } else if (bodyId === "page-login-index") {
+        linkUrl = "https://lms.waomirai.com/login/index.php?tenant=lmswaomirai";
       } else if (bodyId === "page-login-signup") {
         linkUrl = "https://lms.waomirai.com/signup?tenant=lmswaomirai";
       }
