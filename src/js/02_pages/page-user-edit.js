53e4959368548a478c85f9752e300a7bf78a854c
kentaro kobayashi <kentarokobayashi@MB249FE68F.local>
2026-09-08 09:53:51 +0900

    デザイン修正

--- src/js/02_pages/page-user-edit.js
+++ src/js/02_pages/page-user-edit.js
@@ -14,7 +14,6 @@ if (bodyId === "page-user-edit") { // ページIDが「page-user-edit」の場�
   $("#id_profile_field_address").after(
     '<p class="mirainote-address-note">※入会のタイミング以外でのご入力は、ノートが自動送付されませんので、<br />送付希望の旨を事務局までご連絡ください。</p>'
   );
-
   // ===========================
   // 入会/退会科目の非表示（ユーザのみ）
   // ===========================
--- static/js/lmswaomirai.js
+++ static/js/lmswaomirai.js
@@ -3161,7 +3161,6 @@ if (bodyId === "page-user-edit") { // ページIDが「page-user-edit」の場�
   $("#id_profile_field_address").after(
     '<p class="mirainote-address-note">※入会のタイミング以外でのご入力は、ノートが自動送付されませんので、<br />送付希望の旨を事務局までご連絡ください。</p>'
   );
-
   // ===========================
   // 入会/退会科目の非表示（ユーザのみ）
   // ===========================
@@ -3598,7 +3597,7 @@ if (bodyId === "page-user-edit") { // ページIDが「page-user-edit」の場�
     if (anySelectHidden) {
       $("#id_category_10 > .d-flex").after(`
         <p class="subject-level-note">
-          受講中の科目・レベルを変更する場合は<a href="${UrlSubjectChangeForm}" style=" text-decoration:underline !important; target="_blank">科目変更フォーム</a>から申請をお願いします
+          受講中の科目・レベルを変更する場合は<a href="${UrlSubjectChangeForm}" style="text-decoration:underline !important;" target="_blank">科目変更フォーム</a>から申請をお願いします
         </p>
       `);
     }
